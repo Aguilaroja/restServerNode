f8e5498bf3acc4060140858b44d18cdbaa505bbe
Emmanuel <emmanuel.aguilaroja@gmail.com>
2020-01-06 13:31:59 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://spacebar:dWfPnpPEje2LVkk7@cluster0-6j0uf.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
