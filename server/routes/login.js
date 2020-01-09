24a2f848a5408f2e7869da604150d09fbb59c150
Emmanuel <emmanuel.aguilaroja@gmail.com>
2020-01-09 15:11:41 -0600

    Cambio de POST por GET v1.1.0

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,6 +6,129 @@ const app = express();
 const Usuario = require('../models/usuario'); //Ésto es un objeto para el Schema
 const TokenLogin = require('../models/token_login'); //Ésto es un objeto para el Schema
 
+app.post('/login', (req, res) => {
+    let dato = req.body;
+    let passwordIn = String(dato.password);
+
+    return res.json({
+        dato
+    });
+
+    // return res.json({
+    //     // dato,
+    //     email: dato.email,
+    //     pass: dato.password
+    // });
+
+    //Consulta el usuario
+    Usuario.findOne({ email: dato.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (!usuarioDB) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario* o contraseña incorrectos'
+                }
+            })
+        }
+
+        //Compara que las contraseñas sean iguales ya encriptadas
+        if (!bcrypt.compareSync(passwordIn, usuarioDB.password)) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario o contraseña* incorrectos'
+                }
+            })
+        }
+
+        //Se genera el JWT
+        let token = jwt.sign({
+            user: {
+                name: usuarioDB.nombre,
+                email: usuarioDB.email,
+                role: usuarioDB.role
+            }
+        }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+        TokenLogin.findOne({ email: usuarioDB.email }, (err, usuarioTokenDB) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            if (!usuarioTokenDB) {
+                let tokenLogin = new TokenLogin({
+                    email: usuarioDB.email,
+                    tokenLog: token
+                });
+
+                tokenLogin.save((err, tokenLoginDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            err
+                        })
+                    }
+
+                    res.json({
+                        ok: true,
+                        user: {
+                            name: usuarioDB.nombre,
+                            email: usuarioDB.email,
+                            role: usuarioDB.role
+                        },
+                        token
+                    });
+                });
+            } else {
+                let id = {
+                    _id: usuarioTokenDB._id
+                };
+                let cambiaToken = {
+                    tokenLog: token
+                };
+
+                TokenLogin.findOneAndUpdate(id, cambiaToken, { new: true }, (err, tokenActualizadoDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: false,
+                            err
+                        })
+                    }
+
+                    if (!tokenActualizadoDB) {
+                        return res.status(400).json({
+                            ok: false,
+                            err: 'Token no actualizado'
+                        })
+                    }
+
+                    res.json({
+                        ok: true,
+                        user: {
+                            name: usuarioDB.nombre,
+                            email: usuarioDB.email,
+                            role: usuarioDB.role
+                        },
+                        token: tokenActualizadoDB.tokenLog
+                    });
+                });
+
+            }
+
+        });
+    });
+});
+
 app.get('/login', (req, res) => {
     let dato = req.query;
     let passwordIn = String(dato.password);
@@ -127,7 +250,7 @@ app.get('/login', (req, res) => {
 
         });
     });
-})
+});
 
 
 module.exports = app;
