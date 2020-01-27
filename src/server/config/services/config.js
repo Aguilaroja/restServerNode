477d58ca9c1d2808a66c64b6da69a590330b2608
Emmanuel <emmanuel.aguilaroja@gmail.com>
2020-01-27 15:55:49 -0600

    No funciona

--- src/server/config/services/config.js
+++ src/server/config/services/config.js
@@ -5,52 +5,52 @@ let basePath = path.join(__dirname, '../../../../');
 const env = process.env.NODE_ENV || 'development';
 
 if (env === 'production') {
-  basePath = './';
+    basePath = './';
 }
 const envPath = path.join(basePath, `.env/${env}.env`);
 const envConfig = require('dotenv').config({
-  path: envPath
+    path: envPath
 });
 if (envConfig.error) {
-  throw envConfig.error;
+    throw envConfig.error;
 }
 
 const development = {
-  env,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  url: `http://${process.env.HOST}:${process.env.PORT}`,
-  redisUrl: process.env.REDIS_URL,
-  mongoUrl: `mongodb://${process.env.MONGODB_HOST}`,
-  seed: process.env.SEED,
-  frontendStaticFolder: path.join(basePath + 'src/views')
+    env,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    url: `http://${process.env.HOST}:${process.env.PORT}`,
+    redisUrl: process.env.REDIS_URL,
+    mongoUrl: `mongodb://${process.env.MONGODB_HOST}`,
+    seed: process.env.SEED,
+    frontendStaticFolder: path.join(basePath + 'src/views')
 };
 
 const production = {
-  env,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  url: `http://${process.env.HOST}:${process.env.PORT}`,
-  redisUrl: process.env.REDIS_URL,
-  mongoUrl: `mongodb+srv://${process.env.MONGODB_USER}:${encodeURIComponent(
+    env,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    url: `http://${process.env.HOST}:${process.env.PORT}`,
+    redisUrl: process.env.REDIS_URL,
+    mongoUrl: `mongodb+srv://${process.env.MONGODB_USER}:${encodeURIComponent(
     process.env.MONGODB_PASS
   )}@${process.env.MONGODB_HOST}`,
-  seed: process.env.SEED,
-  caducidadToken: 60 * 60 * 24 * 30 * 30, // 30 dias
-  frontendStaticFolder: path.join(basePath + '/src/views')
+    seed: process.env.SEED,
+    caducidadToken: 60 * 60 * 24 * 30 * 30, // 30 dias
+    frontendStaticFolder: path.join(basePath + '/src/views')
 };
 
 const test = {
-  env,
-  host: process.env.HOST,
-  port: process.env.PORT,
-  url: `http://${process.env.HOST}:${process.env.PORT}`,
-  redisUrl: process.env.REDIS_URL,
-  mongoUrl: `mongodb+srv://${process.env.MONGODB_USER}:${encodeURIComponent(
+    env,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    url: `http://${process.env.HOST}:${process.env.PORT}`,
+    redisUrl: process.env.REDIS_URL,
+    mongoUrl: `mongodb+srv://${process.env.MONGODB_USER}:${encodeURIComponent(
     process.env.MONGODB_PASS
   )}@${process.env.MONGODB_HOST}`,
-  seed: process.env.SEED,
-  frontendStaticFolder: path.join(basePath + 'src/views')
+    seed: process.env.SEED,
+    frontendStaticFolder: path.join(basePath + 'src/views')
 };
 
 const config = { test, development, production };
